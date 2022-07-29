3d7b673117beaffee0703253e8a18cf6277b2e38
its-kayes <kayes.ek8@gmail.com>
2022-07-30 00:40:25 +0600

    Home First Section Create

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import Home from './components/home/Home';
 
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Routes>
+        <Route path="/" element={<Home> </Home>} />
+        {/* <Route path="about" element={} /> */}
+      </Routes>
     </div>
   );
 }
