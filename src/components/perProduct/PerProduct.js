2f0e15b718d2e029d9bd500f392a7370e69828cc
its-kayes <kayes.ek8@gmail.com>
2022-08-06 12:22:07 +0600

    single products page design and data load success

--- src/components/perProduct/PerProduct.js
+++ src/components/perProduct/PerProduct.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaStar, FaStarHalf } from 'react-icons/fa';
 import Description from './Description';
 import Information from './Information';
@@ -12,67 +12,85 @@ import { Link, useParams } from 'react-router-dom';
 const PerProduct = () => {
 
     let { id } = useParams();
+    // console.log(id)
 
 
     const [quantity, setQuantity] = useState(1);
     const [description, setDescription] = useState(true);
     const [information, setInformation] = useState(false);
     const [reviews, setReviews] = useState(false);
-    let [size, setSize] = useState(0);
+    const [size, setSize] = useState(0);
+
+    const [item, setItem] = useState([])
+    const [isLoading, setLoading] = useState(false)
 
 
 
-    let { data, isLoading, refetch } = useQuery('singleproduct', () => fetch(`http://localhost:3000/api/singleproduct?id=${id.id}`).then(res => res.json()))
+    // let { data, isLoading, refetch } = useQuery('singleproduct', () => fetch("http://localhost:5000/foods/idx").then(res => res.json()))
 
-    
-    if (isLoading) {
-        return <Loader> </Loader>
-    }
 
-    if (!data) {
-        return refetch();
-    }
-    console.log("id page entar kore");
+    // if (isLoading) {
+    //     return <Loader> </Loader>
+    // }
 
-    // setTotalPrice(product?.price);
+    // if (!data) {
+    //     return refetch();
+    // }
+    // console.log("id page entar kore");
 
-    // if (product?.products === true) {
+    useEffect(() => {
+        // setLoading(true)
+        fetch(`http://localhost:5000/foods/${id}`, )
+            .then(res => res.json())
+            .then(result => {
+                setItem(result.data)
+                setLoading(false)
+                // console.log(result.data);
+            })
+    }, [id])
+    if (isLoading) return <p className='text-center'>Products Loading...</p>
+    if (!item) return <p>No profile data</p>
+
+    // setTotalPrice(item?.price);
+
+    // if (item?.products === true) {
 
     // }
     // let item = product.products;
+    // console.log(data)
 
-    let product = data?.products;
+    // let product = data?.products;
 
     // console.log(totalPrice);
-    // console.log(product?.products.title);
-    // console.log(item);
+    // console.log(item?.products.title);
+    console.log(item.foodId);
 
 
-    // let addToCart = (name, img, quantity, price) => {
-    //     // event.preventDefault();
+    let addToCart = (name, img, quantity, price) => {
+        // event.preventDefault();
 
-    //     let data = {
-    //         totalPrice: (quantity * price),
-    //         email: user.email,
-    //         quantity: quantity,
-    //         name: name,
-    //         img: img,
-    //         size: 10
-    //     }
-    //     console.log(data);
+        let data = {
+            totalPrice: (quantity * price),
+            email: 2,
+            quantity: quantity,
+            name: name,
+            img: img,
+            size: 10
+        }
+        console.log(data);
 
 
-    //     fetch("http://localhost:3000/api/orders", {
-    //         method: "POST",
-    //         headers: {
-    //             'content-type': 'application/json'
-    //         },
-    //         body: JSON.stringify(data)
-    //     })
-    //         .then(res => res.json())
-    //         .then(result => console.log(result));
+        // fetch("http://localhost:3000/api/orders", {
+        //     method: "POST",
+        //     headers: {
+        //         'content-type': 'application/json'
+        //     },
+        //     body: JSON.stringify(data)
+        // })
+        //     .then(res => res.json())
+        //     .then(result => console.log(result));
 
-    // }
+    }
 
 
     return (
@@ -81,10 +99,13 @@ const PerProduct = () => {
                 <div>
                     <div className=' grid grid-cols-1 lg:grid-cols-2 '>
                         <div className='h-[1000px] bg-[#faf7f2] mr-6 static'>
-                            <div className='w-3/4 mt-12 mx-auto '>
+                            {/* <div className='w-3/4 mt-12 mx-auto '>
                                 <img src="https://themefie.com/html/foodka/assets/img/shop/single-1.png" alt="" />
+                            </div> */}
+                            <div className='mt-12 mx-auto '>
+                                <img className='w-[80%] h-[80%] p-16 items-center' src={item?.img} alt="" />
                             </div>
-                            <div className='sm:flex justify-around w-2/4 mt-8 mx-auto'>
+                            <div className='sm:flex justify-between w-3/4 mt-8 mx-auto'>
                                 <img src="https://themefie.com/html/foodka/assets/img/shop/2.png" alt="" />
                                 <img src="https://themefie.com/html/foodka/assets/img/shop/3.png" alt="" />
                                 <img src="https://themefie.com/html/foodka/assets/img/shop/1.png" alt="" />
@@ -92,19 +113,19 @@ const PerProduct = () => {
                         </div>
                         <div className='lg:w-3/4 scroll-my-80'>
                             {/* <h1 className='text-5xl'>Maxican Pizza Test Better</h1> */}
-                            <h1 className='text-5xl'>{product?.title}</h1>
+                            <h1 className='text-5xl'>{item?.title}</h1>
                             <div className='flex justify-between items-center'>
-                                <p className='text-2xl font-bold'>${product?.price}</p>
+                                <p className='text-2xl font-bold'>${item?.price}</p>
                                 {/* <p className='text-2xl font-bold'>${data.price[size]}</p> */}
 
                                 <div className='flex items-center my-12'>
-                                    <p className='flex items-center'><span>{product?.rating}</span> <FaStar className='text-[#d80027]' /></p>
+                                    <p className='flex items-center'><span>{item?.rating}</span> <FaStar className='text-[#d80027]' /></p>
                                     <FaStar className='text-[#d80027]' /><FaStar className='text-[#d80027]' />
                                     <p> <FaStarHalf className='text-[#d80027]' /></p>
                                 </div>
                             </div>
-                            {/* <p>Pizza is a savory dish of Italian origin consisting of a usually round, flattened base of leavened wheat-based dough topped with tomatoes, cheese, and often various other ingredients, which is then baked at a high temperature, traditionally in a wood-fired oven. A small pizza is sometimes called a pizzetta.</p> */}
-                            <p> {product?.desc} </p>
+                            <p>Pizza is a savory dish of Italian origin consisting of a usually round, flattened base of leavened wheat-based dough topped with tomatoes, cheese, and often various other ingredients, which is then baked at a high temperature, traditionally in a wood-fired oven. A small pizza is sometimes called a pizzetta.</p>
+                            <p> {item?.desc} </p>
 
                             <div className='grid grid-cols-2 mt-8'>
                                 <div>
@@ -132,7 +153,7 @@ const PerProduct = () => {
                                 </div> */}
                                 <div>
                                     <p className='text-2xl  font-semibold'> Total Cost</p>
-                                    <input type="number" name="totalPrice" id="" value={quantity * product?.price} className="border-0 rounded-full focus:border-0 bg-[#faf7f2] mt-6 h-10 select-bordered w-2/4 font-bold text-red-600 text-center max-w-xs" />
+                                    <input type="number" name="totalPrice" id="" value={quantity * item?.price} className="border-0 rounded-full focus:border-0 bg-[#faf7f2] mt-6 h-10 select-bordered w-2/4 font-bold text-red-600 text-center max-w-xs" />
                                 </div>
                             </div>
 
@@ -146,9 +167,8 @@ const PerProduct = () => {
                                     <input value={quantity} className=' font-bold w-1/4 text-center p-3 bg-[#faf7f2]' type="number" name="quantity" id="" />
                                     <button onClick={() => setQuantity(quantity + 1)} className='p-3 bg-[#faf7f2] font-bold'>+</button>
                                 </div>
-                                {/* <button onClick={() => addToCart(product?.title, product?.img, quantity, product?.price)} className="btn btn-warning  p-1 font-bold w-[200px]">ADD TO CART</button> */}
+                                <button onClick={() => addToCart(item?.title, item?.img, quantity, item?.price)} className="btn btn-warning  p-1 font-bold w-[200px]">ADD TO CART</button>
                             </div>
-                            {/* <p> Total Price:- { quantity * product?.price } </p> */}
 
                             <hr className='my-6' />
                             <div className='text-gray-400'>
